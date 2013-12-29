ddac5376452f2fefbe9d12bd834f88f4f9c9b726
John Stäck <stack@spotify.com>
2013-12-29 01:24:34 +0100

    Using sox to convert audio format into something taranis can play. Various small fixes.

--- README.md
+++ README.md
@@ -13,6 +13,11 @@ Simple usage:
 The csv file has two columns, the first is output filename and the second is the text to say. It uses Mac OS built in text-to-speech command "say". It is recommended that you download one of the "high-quality" voices available in newer releases. More info on this here:
 http://www.macobserver.com/tmo/article/how-to-install-and-use-high-quality-system-voices-in-os-x
 
-This has only been tested on MacOS 10.9 (Mavericks), but it should work with 10.6 and later.
+This has only been tested on MacOS 10.9 (Mavericks), but it should work with anything after 10.6 at least.
 
-Output directory, output format, CSV separator and voice (if other than default) can be adjusted in the script.
+Requires sox (http://sox.sourceforge.net/) for audio format conversion, easily installed via Homebrew (http://brew.sh/):
+brew install sox
+
+Even if you don't care for the normalizing and silence-trimming, the WAV files output by say (or afconvert) for some reason will not play properly in the Taranis.
+
+Output directory, output format, CSV separator and voice can all be adjusted in the script.
--- mkvoices.sh
+++ mkvoices.sh
@@ -6,18 +6,24 @@ OUTDIR=$(dirname $0)/SOUNDS/en
 #Use a voice other than system default
 #VOICE="Allison"
 
-#Output sound formats.
-#More info on output formats can be found in "afconvert -hf". "say" seems to use the same formats.
+#Sound output format to sox
 
-#Highest quality for Taranis:
-FORMAT="I16@32000"
+#High-quality for Taranis. Note that say seems to just output 22kHz/16bits. While voice usually doesn't go much above 4-5kHz,
+#32kHz is what is shipped default, might as well stich to that. In parenthesis because bash.
+FORMAT=(--bits 16 --channels 1 --encoding signed-integer --rate 32k)
 
-#Lower quality for other OpenTX radios:
-#FORMAT="ulaw@8000"
+#Lower quality for other radios
+#FORMAT=(--bits 8 --channels 1 --encoding a-law --rate 8k)
 
+#sox "effects" to trim silence and normalize sound level
+EFFECTS=(silence 1 0.1 1% gain -n -1)
 
 #Separator character in CSV. Some spreadsheet apps use semicolon instead, can be changed here.
 IFS=,
+
+#Location of binaries. Especially sox may be more or less wherever.
+SAYCMD=/usr/bin/say
+SOXCMD=/usr/local/bin/sox
 
 VOICE_PARAM=""
 if [[ -n "$VOICE" ]]; then VOICE_PARAM="--voice=\"$VOICE\""; fi
@@ -33,7 +39,12 @@ do
 	#Ensure output directory exists for each file if it is in a subdirectory
 	mkdir -p $(dirname $DEST)
 
-	/usr/bin/say $VOICE_PARAM --output-file="$DEST" \
-	             --file-format=WAVE --data-format=$FORMAT --channels=1 \
-	             "\"$SAYING\""
+	#Make a tempfile with .aiff appended, since say will do that automatically otherwise
+	TMPFILE=$(mktemp -ut makesounds).aiff
+
+	$SAYCMD $VOICE_PARAM --output-file="$TMPFILE" \"$SAYING\"
+	$SOXCMD -q "$TMPFILE" ${FORMAT[@]} "$DEST" ${EFFECTS[@]}
+
+	rm $TMPFILE
+	
 done
